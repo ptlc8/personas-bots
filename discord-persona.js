6964d3e1698877ac4a7ac4b953f0c7dfd6e99270
Ambi <ptlc.ptlc.ptlc@gmail.com>
2022-11-10 03:37:07 +0100

    support des emojis de serveur

--- discord-persona.js
+++ discord-persona.js
@@ -18,6 +18,7 @@ class DiscordPersona {
         this.delay = delay || 0;
         /** @type number */
         this.typingTime = typingTime || 2000;
+        /** @type Discord.Client */
         this.client = new Discord.Client({
             intents:
                 [
@@ -43,10 +44,15 @@ class DiscordPersona {
             // else let persona respond
             var response = this.persona.onMessage(message.content, message.mentions.users.has(this.client.user.id));
             if (response) {
-                setTimeout(() => {
-                    message.channel.sendTyping();
-                    setTimeout(() => message.channel.send(response), this.typingTime);
-                }, this.delay);
+                replaceAsync(response, /:([a-zA-Z0-9_]+):/g, async (_match, emojiName) => {
+                    console.log(getEmoji(message.channel.guild, emojiName))
+                    return await getEmoji(message.channel.guild, emojiName);
+                }).then(response => {
+                    setTimeout(() => {
+                        message.channel.sendTyping();
+                        setTimeout(() => message.channel.send(response), this.typingTime);
+                    }, this.delay);
+                });
             }
         });
         this.client.login(token);
@@ -64,6 +70,48 @@ class DiscordPersona {
         }
         return null;
     }
+}
+
+/** @type {{Discord.Snowflake: Discord.Collection<Discord.Snowflake, Discord.GuildEmoji>}}} */
+const emojisCache = {}
+/**
+ * Find emote tag with name
+ * @param {Discord.Guild} guild 
+ * @param {string} emojiName 
+ * @returns {Promise<string>}
+ */
+function getEmoji(guild, emojiName) {
+    return new Promise((resolve, _reject) => {
+        if (!emojisCache[guild.id]) {
+            guild.emojis.fetch()
+                .then(emojis => {
+                    emojisCache[guild.id] = emojis.filter(emoji => emoji.available);
+                    var emoji = emojisCache[guild.id].find(emoji => emoji.name == emojiName);
+                    resolve(emoji ? `<:${emoji.name}:${emoji.id}>` : emojiName);
+                });
+        } else {
+            var emoji = emojisCache[guild.id].find(emoji => emoji.name == emojiName);
+            resolve(emoji ? `<:${emoji.name}:${emoji.id}>` : emojiName);
+        }
+    });
+}
+
+/**
+ * String.replace function but for async function
+ * @see {@link String.replace}
+ * @param {string} str 
+ * @param {string|RegExp} regex 
+ * @param {function(string):Promise<string>} asyncFn 
+ * @returns 
+ */
+async function replaceAsync(str, regex, asyncFn) {
+    const promises = [];
+    str.replace(regex, (match, ...args) => {
+        const promise = asyncFn(match, ...args);
+        promises.push(promise);
+    });
+    const data = await Promise.all(promises);
+    return str.replace(regex, () => data.shift());
 }
 
 module.exports = DiscordPersona;

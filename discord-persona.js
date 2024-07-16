d0f0f4081e19a4922618528c478fc2ea5fd3a502
Ambi <ptlc.ptlc.ptlc@gmail.com>
2024-07-16 22:57:02 +0200

    fix types

--- discord-persona.js
+++ discord-persona.js
@@ -2,10 +2,12 @@ const Discord = require("discord.js");
 const Persona = require("./persona");
 
 /**
- * @typedef {Persona.Config} Config
+ * @typedef DiscordConfig
  * @property {string} token Discord bot token
  * @property {number?} delay Delay before response
  * @property {number?} typingTime Time to type message
+ * 
+ * @typedef {Persona.Config & DiscordConfig} Config
  */
 
 /**
@@ -21,13 +23,13 @@ class DiscordPersona {
     constructor({ token, config, responses, routines, delay, typingTime, ignoreChannels }) {
         /** @type {string} */
         this.config = config;
-        /** @type Persona */
+        /** @type {Persona} */
         this.persona = new Persona({ config, responses, routines, ignoreChannels });
-        /** @type number */
+        /** @type {number} */
         this.delay = delay || 0;
-        /** @type number */
+        /** @type {number} */
         this.typingTime = typingTime || 2000;
-        /** @type Discord.Client */
+        /** @type {Discord.Client} */
         this.client = new Discord.Client({
             intents:
                 [
@@ -37,43 +39,43 @@ class DiscordPersona {
                 ]
         });
         this.client.on("ready", () => {
-            console.info(`[${this.config}] Logged in Discord as ${this.client.user.tag}`);
+            console.info(`[${this.config}] Logged in Discord as ${this.client.user?.tag}`);
         });
         this.client.on("messageCreate", async message => {
             // if it's its message ignore
-            if (message.author.id == this.client.user.id) return;
+            if (message.author.id == this.client.user?.id) return;
             // else if it's command
-            if (message.content.split(" ")[0] == `<@${this.client.user.id}>`) {
-                let result = this.command(message.content.replace(`<@${this.client.user.id}>`, "").trim());
+            if (message.content.split(" ")[0] == `<@${this.client.user?.id}>`) {
+                let result = this.command(message.content.replace(`<@${this.client.user?.id}>`, "").trim());
                 if (result) {
                     message.channel.send(result);
                     return;
                 }
             }
             // else let persona respond
-            var response = this.persona.onMessage(message.content, message.channel.name, message.mentions.users.has(this.client.user.id));
+            var response = this.persona.onMessage(message.content, "name" in message.channel ? message.channel.name : "", message.mentions.users.has(this.client.user?.id ?? "0"));
             if (response) {
                 this.sendMessage(message.channel, response);
             }
         });
         this.client.login(token);
         setInterval(() => {
-            var action = this.persona.onMinute(this.client.channels.cache.filter(channel => channel.send).map(channel => channel.name));
+            let action = this.persona.onMinute(this.client.channels.cache.filter(channel => "messages" in channel).map(channel => "name" in channel ? channel.name : ""));
             if (!action) return;
             if (action.message && action.channel) {
-                this.sendMessage(this.client.channels.cache.find(channel => channel.name == action.channel), action.message);
+                this.sendMessage(this.client.channels.cache.filter(channel => "messages" in channel).find(channel => "name" in channel && channel.name == action.channel) ?? null, action.message);
             }
         }, 60 * 1000);
     }
     /**
      * Send a message in a specific channel with typing
-     * @param {Discord.Channel} channel 
+     * @param {Discord.TextBasedChannel?} channel 
      * @param {string|String[]} message 
      */
-    sendMessage(channel, message) {
-        if (!channel.send)
+    async sendMessage(channel, message) {
+        if (!channel)
             return;
-        if (!channel.permissionsFor(channel.guild.members.me).has([Discord.PermissionFlagsBits.SendMessages, Discord.PermissionFlagsBits.ViewChannel]))
+        if ("guild" in channel && channel.guild.members.me && !channel.permissionsFor(channel.guild.members.me).has([Discord.PermissionFlagsBits.SendMessages, Discord.PermissionFlagsBits.ViewChannel]))
             return;
         if (message instanceof Array) {
             if (message.length == 0) return;
@@ -81,14 +83,14 @@ class DiscordPersona {
             setTimeout(() => this.sendMessage(channel, remain), randomize(this.delay / 2, 20) + randomize(this.typingTime, 10) + 100);
             message = message[0];
         }
-        replaceAsync(message, /(?<!<):([a-zA-Z0-9_]+):(?![0-9])/g, async (_match, emojiName) => {
-            return await getEmoji(channel.guild, emojiName);
-        }).then(message => {
-            setTimeout(() => {
-                channel.sendTyping();
-                setTimeout(() => channel.send(message), randomize(this.typingTime, 30));
-            }, randomize(this.delay, 60));
-        });
+        if ("guild" in channel)
+            message = await replaceAsync(message, /(?<!<):([a-zA-Z0-9_]+):(?![0-9])/g, async (_match, emojiName) => {
+                return await getEmoji(channel.guild, emojiName);
+            });
+        setTimeout(() => {
+            channel.sendTyping();
+            setTimeout(() => channel.send(message), randomize(this.typingTime, 30));
+        }, randomize(this.delay, 60));
     }
     /**
      * Get result of a command
@@ -105,7 +107,7 @@ class DiscordPersona {
     }
 }
 
-/** @type {{Discord.Snowflake: Discord.Collection<Discord.Snowflake, Discord.GuildEmoji>}}} */
+/** @type {Object.<string, Discord.Collection<Discord.Snowflake, Discord.GuildEmoji>>} */
 const emojisCache = {}
 /**
  * Find emote tag with name
@@ -118,7 +120,7 @@ function getEmoji(guild, emojiName) {
         if (!emojisCache[guild.id]) {
             guild.emojis.fetch()
                 .then(emojis => {
-                    emojisCache[guild.id] = emojis.filter(emoji => emoji.available);
+                    emojisCache[guild.id] = emojis.filter(emoji => emoji.available ?? false);
                     var emoji = emojisCache[guild.id].find(emoji => emoji.name == emojiName);
                     resolve(emoji ? `<:${emoji.name}:${emoji.id}>` : emojiName);
                 });
@@ -134,14 +136,16 @@ function getEmoji(guild, emojiName) {
  * @see {@link String.replace}
  * @param {string} str 
  * @param {string|RegExp} regex 
- * @param {function(string):Promise<string>} asyncFn 
+ * @param {function(...string):Promise<string>} asyncFn 
  * @returns 
  */
 async function replaceAsync(str, regex, asyncFn) {
+    /** @type {Promise<?>[]} */
     const promises = [];
     str.replace(regex, (match, ...args) => {
         const promise = asyncFn(match, ...args);
         promises.push(promise);
+        return "";
     });
     const data = await Promise.all(promises);
     return str.replace(regex, () => data.shift());
--- persona.js
+++ persona.js
@@ -1,9 +1,8 @@
 /**
  * @typedef Response
- * @type {object}
  * @property {string?} pattern pattern to match with previous message to send
  * @property {number?} frequence frequence between 0 and 1 of response
- * @property {string[]|string[][]?} expressions array of possible expressions
+ * @property {(string|string[])[]?} expressions array of possible expressions
  * @property {string|string[]?} expression only if one expression
  * @property {boolean} whenMention only use when mention
  * @property {string[]?} channels array of channel id allowed, default is all
@@ -11,18 +10,16 @@
 
 /**
  * @typedef Routine
- * @type {object}
  * @property {[string,string]?} between two time between routine can be sent, example : ["10:00", "18:00"]
  * @property {number?} frequence probability of it being sent every minute
- * @property {string[]?} expressions array of possible expressions
- * @property {string?} expression only if one expression
+ * @property {(string|string[])[]?} expressions array of possible expressions
+ * @property {(string|string[])?} expression only if one expression
  * @property {string[]?} channels array of channel id allowed, default is all
  */
 
 /**
  * @typedef Config
- * @type {object}
- * @property {string?} config configuration name
+ * @property {string} config configuration name
  * @property {Response[]?} responses possible responses
  * @property {Routine[]?} routines possible routines
  * @property {string[]?} ignoreChannels channels names patterns to ignore
@@ -33,7 +30,7 @@ class Persona {
      * @param {Config} config
      */
     constructor({ config, responses, routines, ignoreChannels }) {
-        /** @type {string?} */
+        /** @type {string} */
         this.config = config;
         /** @type {Response[]} */
         this.responses = responses || [];
@@ -49,9 +46,9 @@ class Persona {
      * @param {boolean} mentioned true if the persona was mentioned
      * @returns {(string|string[])?} response of the persona
      */
-    onMessage(message, channel = undefined, mentioned = false) {
+    onMessage(message, channel = "", mentioned = false) {
         if (!filterChannels([channel], this.ignoreChannels)[0])
-            return;
+            return null;
         for (let response of this.responses) {
             if (response.channels && !response.channels.includes(channel)) continue;
             if (!(response.whenMention && mentioned) && (Math.random() > (response.frequence || 0))) continue;
@@ -59,28 +56,30 @@ class Persona {
                 ? message.match(new RegExp(response.pattern, "i"))
                 : [message];
             if (match) {
-                let expression = response.expressions ? pickRandom(response.expressions) : response.expression;
-                return expression instanceof Array ? expression.map(e => format(e, match)) : format(expression, match);
+                let expression = response.expressions ? pickRandom(response.expressions) : response.expression ?? [];
+                return expression instanceof Array ? expression.map(e => format(e, match ?? [])) : format(expression, match);
             }
         }
+        return null;
     }
     /**
      * Inform the persona that a minute has passed
      * @param {string[]} channels list of channels names or identifiers
-     * @returns {{message: string | string[], channel: string?}} message the persona would send
+     * @returns {{message: string | string[], channel: string?}?} message the persona would send
      */
     onMinute(channels) {
         for (let routine of this.routines) {
             if (!testBetween(routine.between) || (Math.random() > (routine.frequence || 0))) continue;
             return {
-                message: routine.expressions ? pickRandom(routine.expressions) : routine.expression,
-                channel: pickRandom(filterChannels(channels, this.ignoreChannels, routine.channels))
+                message: routine.expressions ? pickRandom(routine.expressions) : routine.expression ?? [],
+                channel: pickRandom(filterChannels(channels, this.ignoreChannels, routine.channels ?? []))
             };
         }
+        return null;
     }
     /**
      * Get info about the persona
-     * @returns {{expressions: number, frequence: number, config: string, responses: number}}
+     * @returns {{expressions: number, frequence: number, config: string, responses: number, routines: number}}
      */
     info() {
         return {
@@ -100,7 +99,7 @@ class Persona {
  */
 function format(str, args) {
     return str.replace(/{([0-9]+)}/g, function (match, index) {
-        return typeof args[index] == 'undefined' ? match : args[index];
+        return typeof args[index] == "undefined" ? match : args[index];
     });
 }
 
@@ -115,7 +114,7 @@ function pickRandom(arr) {
 
 /**
  * Test if actual time is between interval
- * @param {[string,string]} interval two time between routine can be sent, example : ["10:00", "18:00"]
+ * @param {[string,string]?} interval two time between routine can be sent, example : ["10:00", "18:00"]
  * @returns {boolean} 
  */
 function testBetween(interval) {
@@ -133,12 +132,11 @@ function testBetween(interval) {
  * @param {string[]} patterns inclusion patterns, if undefined, all channels are included
  * @returns {string[]} filtered channels
  */
-function filterChannels(channels, ignorePatterns, patterns = undefined) {
+function filterChannels(channels, ignorePatterns, patterns = []) {
     return channels.filter(channel => {
         for (let iPattern of ignorePatterns)
             if (channel.match(new RegExp(iPattern, "i")))
                 return false;
-        if (!patterns) return true;
         for (let pattern of patterns)
             if (channel.match(new RegExp(pattern, "i")))
                 return true;
